a9f9ec196c30d607041b13ee5f9b762d83c7e632
vsoulard@wanadoo.fr <Vince@iMac-de-iMac.local>
2020-05-13 12:38:29 +0200

    V1

--- Rational.py
+++ Rational.py
@@ -7,6 +7,17 @@ class Rational(object):
 			raise BaseExecption( "Denominator cannot be null" )
 		self.numerator = num
 		self.denominator = den
+		self.simplify()
 
 	def __str__( self ):
 		return "[%d/%d]" % ( self.numerator, self.denominator )
+
+	def simplify(self):
+		divisor = 2
+		# recherche du plus petit diviseur commun
+		while divisor <= min(self.numerator, self.denominator):
+			while self.numerator % divisor == 0 and self.denominator % divisor == 0:
+				self.numerator /= divisor
+				self.denominator /= divisor
+			divisor += 1
+			
--- Start.py
+++ Start.py
@@ -3,5 +3,14 @@
 from Rational import Rational
 
 r = Rational( 3, 2 )
+r1 = Rational( 4, 8 )
+
 print ( r )
-#r = Rational( 3, 0 )
+print ( r1 )
+
+r1.numerator = 4
+r1.denominator = 16
+r1.simplify()
+
+print ( r1 )
+
