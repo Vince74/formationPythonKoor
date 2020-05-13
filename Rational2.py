804d077cf614875204851bfda4176b819a23a1f9
vsoulard@wanadoo.fr <Vince@iMac-de-iMac.local>
2020-05-13 17:13:56 +0200

    V2 encapsulation et propriété

--- Rational2.py
+++ Rational2.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
 
-class Rational(object):
+class Rational2(object):
 
 	#-----------------------------------------------
 	# constructor
 	#-----------------------------------------------
 	def __init__( self, newNumerator, newDenominator ):
-		self.__setNumerator(newNumerator);
-		self.__setDenominator(newDenominator);
+		self.nominator = newNominator
+		self.denominator = newDenominator
 		self.simplify()
 
 	#-----------------------------------------------
 	# setter / getter
 	#-----------------------------------------------
-	def __getNumerator(self):
+
+	@property
+	def numerator(self):
 		return self.__numerator
 
-	def __setNumerator(self, newNumerator):
+	@numerator.setter
+	def numerator(self, newNumerator):
 		if isinstance(newNumerator,int) == False:
 			raise BaseExecption( "Numerator must be integer" )
 		self.__numerator = newNumerator
 
-	def __getDenominator(self):
+	@property
+	def denominator(self):
 		return self.__denominator
 
-	def __setDenominator(self, newDenominator):
+	@denominator.setter
+	def denominator(self, newDenominator):
 		if isinstance(newDenominator,int) == False:
 			raise BaseExecption( "Denominator must be integer" )
 		if newDenominator == 0:
 			raise BaseExecption( "Denominator cannot be null" )
 		self.__denominator = newDenominator
-
-	#---------------------------------------------------------------------
-	# Property: paire de methodes (getter + setter) d'acces a un attribut
-	#---------------------------------------------------------------------
-	numerator = property( __getNumerator, __setNumerator )
-	denominator = property( __getDenominator, __setDenominator )
 
 	#-----------------------------------------------
 	# methods
--- Start.py
+++ Start.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 
 from Rational import Rational
+from Rational2 import Rational2
 
-r = Rational( 3, 2 )
-r1 = Rational( 4, 8 )
+# class Ratrional
 
-print ( r )
+r1 = Rational( 3, 2 )
+r2 = Rational( 4, 8 )
+
+print ( r1 )
+print ( r2 )
+
+r1.numerator = 12;
+r1.denominator = 24;
 print ( r1 )
 
-r1.numerator = 4
-r1.denominator = 16
 r1.simplify()
-
 print ( r1 )
 
+# class Ratrional2
+
+r21 = Rational( 3, 2 )
+r22 = Rational( 4, 8 )
+
+print ( r21 )
+print ( r22 )
+
+r21.numerator = 12;
+r21.denominator = 24;
+print ( r21 )
+
+r21.simplify()
+print ( r21 )
